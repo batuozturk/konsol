4a4bca5e97e1af28fcbd86309bee2a6e1856ff46
Batuhan Öztürk <bozturk1999@gmail.com>
2023-04-17 22:43:14 +0300

    auth repository implementation

---
+++ core/src/main/java/com/batuhan/core/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.batuhan.core.data.repository
+
+import com.batuhan.core.data.OauthUser
+import com.batuhan.core.data.source.local.AuthLocalDataSource
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(private val localDataSource: AuthLocalDataSource) :
+    AuthRepository {
+    override suspend fun getAuthenticatedUser(): OauthUser {
+        return localDataSource.getAuthenticatedUser()
+    }
+
+    override suspend fun addAuthenticatedUser(authState: OauthUser) {
+        return localDataSource.addAuthenticatedUser(authState)
+    }
+
+    override suspend fun deleteAuthenticatedUser(oauthUser: OauthUser) {
+        return localDataSource.deleteAuthenticatedUser(oauthUser)
+    }
+}
