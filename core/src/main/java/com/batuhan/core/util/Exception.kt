7c5b9e6887d785733fe4d9cf6935a91c7468a022
Batuhan Öztürk <bozturk1999@gmail.com>
2023-05-04 01:50:43 +0300

    result and exception classes updated

--- core/src/main/java/com/batuhan/core/util/Exception.kt
+++
@@ -1,9 +0,0 @@
-package com.batuhan.core.util
-
-enum class Exception(val code: String?, val message: String?) {
-    // Firebase Exceptions
-
-    // AppAuth Exceptions
-
-    // Other Exceptions
-}
--- core/src/main/java/com/batuhan/core/util/Result.kt
+++ core/src/main/java/com/batuhan/core/util/Result.kt
@@ -2,5 +2,5 @@ package com.batuhan.core.util
 
 sealed class Result<out T> {
     data class Success<out R>(val data: R) : Result<R>()
-    data class Error(val exception: Exception) : Result<Nothing>()
+    data class Error(val exceptionType: ExceptionType, val throwable: Throwable) : Result<Nothing>()
 }
