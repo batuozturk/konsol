e66c79c872047ffcf0bfd8674a0cdf9d462fa328
Batuhan Öztürk <bozturk1999@gmail.com>
2023-05-17 22:25:21 +0300

    updated auth state manager for refresh token logic

--- core/src/main/java/com/batuhan/core/util/AuthStateManager.kt
+++ core/src/main/java/com/batuhan/core/util/AuthStateManager.kt
@@ -1,17 +1,25 @@
 package com.batuhan.core.util
 
+import android.content.Context
 import com.batuhan.core.data.OauthUser
-import com.batuhan.core.domain.AddAuthenticatedUser
-import com.batuhan.core.domain.DeleteAuthenticatedUser
-import com.batuhan.core.domain.GetAuthenticatedUser
+import com.batuhan.core.domain.*
+import dagger.hilt.android.qualifiers.ApplicationContext
 import net.openid.appauth.AuthState
+import net.openid.appauth.AuthorizationService
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AuthStateManager @Inject constructor(
     private val addAuthenticatedUser: AddAuthenticatedUser,
     private val getAuthenticatedUser: GetAuthenticatedUser,
-    private val deleteAuthenticatedUser: DeleteAuthenticatedUser
+    private val deleteAuthenticatedUser: DeleteAuthenticatedUser,
+    private val updateAuthenticatedUser: UpdateAuthenticatedUser,
+    private val refreshToken: RefreshToken,
+    @ApplicationContext private val context: Context
 ) {
+
+    private var oauthUser: OauthUser? = null
 
     suspend fun getAuthState(): Result<OauthUser?> {
         return getAuthenticatedUser.invoke()
@@ -22,6 +30,38 @@ class AuthStateManager @Inject constructor(
 
     suspend fun addAuthState(authState: AuthState): Result<Unit> {
         val oauthUser = OauthUser(authState = authState)
+        setAuthState(oauthUser)
         return addAuthenticatedUser.invoke(AddAuthenticatedUser.Params(oauthUser))
     }
+
+    fun setAuthState(oauthUser: OauthUser) {
+        this.oauthUser = oauthUser
+    }
+
+    fun clearAuthState() {
+        oauthUser = null
+    }
+
+    suspend fun updateAuthState(result: Result<AuthState>): Result<Unit> {
+        return when (result) {
+            is Result.Error -> result
+            is Result.Success -> {
+                return oauthUser?.copy(authState = result.data)?.let {
+                    setAuthState(it)
+                    updateAuthenticatedUser.invoke(UpdateAuthenticatedUser.Params(it))
+                } ?: Result.Error(ExceptionType.ROOM_DB_ERROR, Throwable())
+            }
+        }
+    }
+
+    suspend fun refreshToken(): String? {
+        oauthUser?.authState?.takeIf { it.needsTokenRefresh }?.let {
+            val params = RefreshToken.Params(it, AuthorizationService(context))
+            val tokenRefreshResult = refreshToken.invoke(params)
+            updateAuthState(tokenRefreshResult)
+        }
+        return getAccessToken()
+    }
+
+    fun getAccessToken() = oauthUser!!.authState.accessToken
 }
