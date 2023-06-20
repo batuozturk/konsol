5d92b7270b215e63c85f0ef32fa655629d78e6ed
Batuhan Öztürk <bozturk1999@gmail.com>
2023-06-20 23:31:47 +0300

    firebase project template data class deleted

--- firebase/management/src/main/java/com/batuhan/management/data/model/FirebaseProjectTemplate.kt
+++
@@ -1,8 +0,0 @@
-package com.batuhan.management.data.model
-
-data class FirebaseProjectTemplate(
-    var isCreatingFromScratch: Boolean = false,
-    var createProjectRequest: CreateProjectRequest? = null,
-    var analyticsAccountId: String? = null,
-    var locationId: String? = null
-)
