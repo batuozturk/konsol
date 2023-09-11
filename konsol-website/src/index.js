4aca67674adc2313306dbeef4e8e561b12e99350
Batuhan Öztürk <bozturk1999@gmail.com>
2023-09-12 01:07:38 +0300

    konsol website added, will be fixed in the future

---
+++ konsol-website/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+import NunitoGoogle from './fonts/Nunito-Regular.ttf';
+
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: 'NunitoGoogle',
+      textTransform: 'none',
+      fontWeight: 100,
+      fontSize: 24,
+    },
+    fontWeight: 100
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Nunito';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: url(${NunitoGoogle}) format('ttf');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ konsol-website/src/pages/Home.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="Home">
+    </div>
+  );
+}
+
+export default Home;
