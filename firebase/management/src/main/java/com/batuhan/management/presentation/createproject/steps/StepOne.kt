53aab858f1b27e2cd35db59f2b3600ad3e28f812
Batuhan Öztürk <bozturk1999@gmail.com>
2023-09-16 03:12:12 +0300

    create project step one screen ui fixes

--- firebase/management/src/main/java/com/batuhan/management/presentation/createproject/steps/StepOne.kt
+++ firebase/management/src/main/java/com/batuhan/management/presentation/createproject/steps/StepOne.kt
@@ -4,12 +4,18 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircleOutline
+import androidx.compose.material.icons.filled.Cloud
+import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,6 +63,8 @@ fun StepOneContent(
                 .border(2.dp, Orange, RoundedCornerShape(10.dp))
                 .padding(10.dp),
             title = stringResource(id = R.string.step_one_choice_one),
+            description = stringResource(id = R.string.step_one_choice_one_description),
+            icon = Icons.Default.Cloud,
             isSelected = stepOneState.isCreatingFromScratch == false,
             saveFirstStep = {
                 saveFirstStep(false)
@@ -71,6 +79,8 @@ fun StepOneContent(
                 .border(2.dp, Orange, RoundedCornerShape(10.dp))
                 .padding(10.dp),
             title = stringResource(id = R.string.step_one_choice_two),
+            description = stringResource(id = R.string.step_one_choice_two_description),
+            icon = Icons.Default.Add,
             isSelected = stepOneState.isCreatingFromScratch == true,
             saveFirstStep = {
                 saveFirstStep(true)
@@ -83,7 +93,9 @@ fun StepOneContent(
 fun StepOneItem(
     modifier: Modifier = Modifier,
     title: String,
+    description: String,
     isSelected: Boolean,
+    icon: ImageVector,
     saveFirstStep: () -> Unit
 ) {
     Row(
@@ -98,10 +110,24 @@ fun StepOneItem(
             },
             colors = RadioButtonDefaults.colors(selectedColor = Orange)
         )
-        Text(
-            text = title,
-            textAlign = TextAlign.Start
-        )
+        Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.SpaceBetween) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = Orange,
+                modifier = Modifier.size(48.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = title,
+                textAlign = TextAlign.Start
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = description,
+                textAlign = TextAlign.Start
+            )
+        }
     }
 }
 
